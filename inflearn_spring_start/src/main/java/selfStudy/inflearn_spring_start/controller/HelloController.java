927748116f84b9e72db9186c67c6af2038dec068
최준혁 <ryan020820@gmail.com>
2026-06-28 01:04:59 +0900

    .

---
+++ inflearn_spring_start/src/main/java/selfStudy/inflearn_spring_start/controller/HelloController.java
@@ -0,0 +1,15 @@
+package selfStudy.inflearn_spring_start.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello spring!!!!");
+        return "hello";
+    }
+}
