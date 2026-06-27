683977a7b7fb05b3e5015f1d004fca3feefef602
최준혁 <ryan020820@gmail.com>
2026-06-27 23:50:57 +0900

    .

---
+++ inflearn_spring_start/src/main/java/selfStudy/inflearn_spring_start/InflearnSpringStartApplication.java
@@ -0,0 +1,15 @@
+package selfStudy.inflearn_spring_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringStartApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(InflearnSpringStartApplication.class, args);
+        
+    }
+
+}
---
+++ inflearn_spring_start/src/test/java/selfStudy/inflearn_spring_start/InflearnSpringStartApplicationTests.java
@@ -0,0 +1,13 @@
+package selfStudy.inflearn_spring_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
