6279c4888e84d48e021edca16df4979e0c09b36b
최준혁 <ryan020820@gmail.com>
2026-06-28 18:06:45 +0900

    .

--- inflearn_spring_start/src/main/java/selfStudy/inflearn_spring_start/controller/HelloController.java
+++ inflearn_spring_start/src/main/java/selfStudy/inflearn_spring_start/controller/HelloController.java
@@ -3,13 +3,57 @@ package selfStudy.inflearn_spring_start.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+// 이 어노테이션이 붙어 있어야 Spring이 이 클래스를 찾아서 요청을 처리하는 컨트롤러로 인식함
 public class HelloController {
 
     @GetMapping("hello")
+    // 웹 브라우저에서 /hello라는 URL로 HTTP GET 요청이 들어오면, 바로 아래에 있는 hello 메서드를 실행
     public String hello(Model model) {
         model.addAttribute("data", "hello spring!!!!");
+        // model 객체에 data라는 이름의 키에 값을 저장한다
         return "hello";
+        // View Resolver에게 hello라는 이름의 화면을 찾아서 띄우라는 것
+    }
+
+    // MVC와 템플릿 엔진 예시
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        // RequestParam - localhost:8080/hello-mvc?name=hi -> 여기서 hi를 꺼내서 name 변수에 저장
+        // Model model: controller -> view로 데이터를 전달하는 객체, 직접 생성하는게 아니라 스프링이 자동으로 만들어서 인자로 넣어줌
+        model.addAttribute("name", name);
+        return "hello-template";
+        // return하면 뷰 리졸버가 html 파일을 찾음
+    }
+
+    // API 예시
+    @GetMapping("hello-string")
+    @ResponseBody
+    // view를 거치지 않고 HTTP의 body에 내가 데이터를 직접 넣어주겠다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
