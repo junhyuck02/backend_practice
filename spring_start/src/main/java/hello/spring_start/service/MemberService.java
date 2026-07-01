3f3747b724ca7ec1e082a553ed3a328db544fb66
최준혁 <ryan020820@gmail.com>
2026-07-01 15:38:29 +0900

    Create MemberService.java

---
+++ spring_start/src/main/java/hello/spring_start/service/MemberService.java
@@ -0,0 +1,39 @@
+package hello.spring_start.service;
+
+import hello.spring_start.domain.Member;
+import hello.spring_start.repository.MemberRepository;
+import hello.spring_start.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new
+            MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+        // ifPresent: 값이 있을 때만 동작을 실행하고 없으면 안함
+        // IllegalStateException: 부적절한 상태일 때 사용
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
