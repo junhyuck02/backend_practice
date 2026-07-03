4ccd3e0f07b055d84eee2c54d5aa9b0f0d67cf86
최준혁 <ryan020820@gmail.com>
2026-07-03 16:47:34 +0900

    Update JdbcMemberRepository.java

--- spring_start/src/main/java/hello/spring_start/repository/JdbcMemberRepository.java
+++ spring_start/src/main/java/hello/spring_start/repository/JdbcMemberRepository.java
@@ -16,20 +16,28 @@ public class JdbcMemberRepository implements MemberRepository {
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+    // ?
 
     @Override
     public Member save(Member member) {
         String sql = "insert into member(name) values(?)";
         Connection conn = null;
+        // ?
         PreparedStatement pstmt = null;
+        // ?
         ResultSet rs = null;
+        // ?
         try {
             conn = getConnection();
-            pstmt = conn.prepareStatement(sql,
-                    Statement.RETURN_GENERATED_KEYS);
+            // ?
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            // ?
             pstmt.setString(1, member.getName());
+            // ?
             pstmt.executeUpdate();
+            // ?
             rs = pstmt.getGeneratedKeys();
+            // ?
             if (rs.next()) {
                 member.setId(rs.getLong(1));
             } else {
@@ -41,6 +49,7 @@ public class JdbcMemberRepository implements MemberRepository {
         } finally {
             close(conn, pstmt, rs);
         }
+        // ????
     }
 
     @Override
