4f0eb0be3c1b414bf2d82e4a880549598a0bb4ba
최준혁 <ryan020820@gmail.com>
2026-07-30 16:33:00 +0900

    .

--- spring_basic/src/test/java/hello/spring_basic/discount/RateDiscountPolicyTest.java
+++ spring_basic/src/test/java/hello/spring_basic/discount/RateDiscountPolicyTest.java
@@ -1,8 +1,37 @@
 package hello.spring_basic.discount;
 
+import hello.spring_basic.member.Grade;
+import hello.spring_basic.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
-    
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
 
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    // 테스트 클래스나 테스트 메서드에 사람이 읽기 좋은 이름(설명)을 붙여주는 기능
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+    void vip_x() {
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
 }
